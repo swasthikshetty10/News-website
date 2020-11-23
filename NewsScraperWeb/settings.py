7a46cd9695001dc0dcde5b6b444fa726f0c06f23
swasthikshetty10 <swasthikshetty10902@gmail.com>
2020-11-23 23:07:13 +0530

    Version 1.0

--- NewsScraperWeb/settings.py
+++ NewsScraperWeb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +125,4 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 
 ]
+django_heroku.settings(locals())
