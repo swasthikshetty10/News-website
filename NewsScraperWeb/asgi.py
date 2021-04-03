c27e169ca4292b84f4356d7a5c41405216d2ffa6
Swasthik Shetty <62538932+swasthikshetty10@users.noreply.github.com>
2021-04-03 17:12:02 +0530

    Update asgi.py

--- NewsScraperWeb/asgi.py
+++ NewsScraperWeb/asgi.py
@@ -1,11 +1,3 @@
-"""
-ASGI config for NewsScraperWeb project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
 
 import os
 
